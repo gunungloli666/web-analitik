ead7a6280c94e85151dc2471d16eb9fcc4dc7722
gunungloli666 <gunungloli@gmail.com>
2020-03-05 21:16:12 +0800

    tambahkan commit

---
+++ src/com/fjr/simulasi/SimulasiSPH.java
@@ -0,0 +1,22 @@
+package com.fjr.simulasi;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SimulasiSPH  extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+		ServletContext sc = getServletContext();
+		RequestDispatcher rd = sc.getRequestDispatcher("/WEB-INF/test.jsp");
+		request.setAttribute("content", "simulasi-sph" );
+
+		try {
+			rd.forward(request, response);
+		} catch (Exception e) {
+			System.out.println("eror dispatching sevlet");
+		}
+	}
+
+}
